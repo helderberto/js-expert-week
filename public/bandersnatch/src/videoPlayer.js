35a824ca0e93ef0c57139eaa06d8fed9eb7084b2
Helder Burato Berto <helder.burato@gmail.com>
2020-10-29 10:33:05 -0300

    add video duration to VideoMediaPlayer

--- public/bandersnatch/src/videoPlayer.js
+++ public/bandersnatch/src/videoPlayer.js
@@ -1,8 +1,11 @@
 class VideoMediaPlayer {
-  constructor({ manifestJSON }) {
+  constructor({ manifestJSON, network }) {
     this.videoElement = null;
     this.sourceBuffer = null;
     this.manifestJSON = manifestJSON;
+    this.network = network;
+    this.select = {};
+    this.videoDuration = 0;
   }
 
   initializeCodec() {
@@ -18,5 +21,41 @@ class VideoMediaPlayer {
       alert(`Seu browser não suporta o codec: ${this.manifestJSON.codec}`);
       return;
     }
+
+    const mediaSource = new MediaSource();
+    this.videoElement.src = URL.createObjectURL(mediaSource);
+
+    mediaSource.addEventListener(
+      "sourceopen",
+      this.sourceOpenWrapper(mediaSource),
+    );
+  }
+
+  sourceOpenWrapper(mediaSource) {
+    return async (_) => {
+      this.sourceBuffer = mediaSource.addSourceBuffer(this.manifestJSON.codec);
+      const selected = (this.selected = this.manifestJSON.intro);
+      // Evita rodar como "LIVE"
+      mediaSource.duration = this.videoDuration;
+      await this.fileDownload(selected.url);
+    };
+  }
+
+  async fileDownload(url) {
+    const prepareUrl = {
+      url,
+      fileResolution: 360,
+      fileResolutionTag: this.manifestJSON.fileResolutionTag,
+      hostTag: this.manifestJSON.hostTag,
+    };
+    const finalUrl = this.network.parseManifestUrl(prepareUrl);
+    this.setVideoPlayerDuration(finalUrl);
+    console.log("videoDuration", this.videoDuration);
+  }
+
+  setVideoPlayerDuration(finalURL) {
+    const bars = finalURL.split("/");
+    const [name, videoDuration] = bars[bars.length - 1].split("-");
+    this.videoDuration += videoDuration;
   }
 }
