632a445c8c594fbab7b97f5e833f2beafdbd9530
Helder Burato Berto <helder.burato@gmail.com>
2020-10-29 10:31:38 -0300

    add network class
    It parses the manifest URL

---
+++ public/bandersnatch/src/network.js
@@ -0,0 +1,11 @@
+class Network {
+  constructor({ host }) {
+    this.host = host;
+  }
+
+  parseManifestUrl({ url, fileResolution, fileResolutionTag, hostTag }) {
+    return url
+      .replace(fileResolutionTag, fileResolution)
+      .replace(hostTag, this.host);
+  }
+}
