a3e86e7f7bc27c65a37ab465cb79ba848eba362a
Helder Burato Berto <helder.burato@gmail.com>
2020-11-05 19:57:41 -0300

    add get proper resolution by duration time of request

--- public/bandersnatch/src/network.js
+++ public/bandersnatch/src/network.js
@@ -13,4 +13,36 @@ class Network {
     const response = await fetch(url);
     return response.arrayBuffer();
   }
+
+  async getProperResolution(url) {
+    const startMs = Date.now();
+    const response = await fetch(url);
+    await response.arrayBuffer();
+    const endMs = Date.now();
+    const durationInMs = endMs - startMs;
+    console.log("durationInMs", durationInMs);
+
+    // instead of calculate throughput we will calculate the time duration
+    const resolutions = [
+      // worst scenario - 20s
+      { start: 3001, end: 20000, resolution: 144 },
+
+      // up to 3s
+      { start: 901, end: 3000, resolution: 360 },
+
+      // less than 1s
+      { start: 0, end: 900, resolution: 720 },
+    ];
+
+    const item = resolutions.find((item) => {
+      return item.start <= durationInMs && item.end >= durationInMs;
+    });
+
+    const LOWEST_RESOLUTION = 144;
+
+    // if it's more than 30 seconds
+    if (!item) return LOWEST_RESOLUTION;
+
+    return item.resolution;
+  }
 }
